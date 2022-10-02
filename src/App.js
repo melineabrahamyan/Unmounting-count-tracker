2f906f89b48390847fbe01ccd6ed5213bb45eb08
melineabrahamyan <abrahamyan.meline@gmail.com>
2022-10-02 19:58:59 +0400

    everything from scratch` unmouted whole component instead of a part

--- src/App.js
+++ src/App.js
@@ -4,33 +4,88 @@ import Card from "./components/card";
 class App extends Component {
   constructor() {
     super();
-    this.state = { showReport: false };
+    this.state = {
+      cards: [
+        { title: 1, isVisible: true, unmountCount: 0 },
+        { title: 2, isVisible: true, unmountCount: 0 },
+        { title: 3, isVisible: true, unmountCount: 0 },
+        { title: 4, isVisible: true, unmountCount: 0 },
+        { title: 5, isVisible: true, unmountCount: 0 },
+      ],
+      showReport: false,
+    };
   }
-  cards = [
-    { title: "1" },
-    { title: "2" },
-    { title: "3" },
-    { title: "4" },
-    { title: "5" },
-  ];
 
-  handleReport = () => {
+  handleIsVisible = (title) => {
+    this.setState({
+      card: this.state.cards.map((card) => {
+        if (card.title === title) {
+          card.isVisible = !card.isVisible;
+        }
+        return card;
+      }),
+    });
+  };
+
+  hanldeUnmountCount = (title) => {
+    this.setState({
+      card: this.state.cards.map((card) => {
+        if (card.title === title) {
+          card.unmountCount = card.unmountCount + 1;
+        }
+        return card;
+      }),
+    });
+  };
+
+  handleShowReport = () => {
     this.setState({ showReport: !this.state.showReport });
   };
 
   render() {
     const { showReport } = this.state;
     return (
-      <div>
-        <div className="container">
-          {this.cards.map((card) => (
-            <Card key={card.title} title={card.title} showReport={showReport} />
-          ))}
-        </div>
-        <button className="report-button" onClick={this.handleReport}>
-          {showReport ? "Close Report" : "Show Report"}
+      <>
+        {this.state.cards.map((card) => {
+          return card.isVisible ? (
+            <>
+              <Card
+                key={card.title}
+                {...card}
+                hanldeUnmountCount={this.hanldeUnmountCount}
+              />
+              <button
+                onClick={() => {
+                  this.handleIsVisible(card.title);
+                }}
+              >
+                Close
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => {
+                this.handleIsVisible(card.title);
+              }}
+            >
+              Show
+            </button>
+          );
+        })}
+        <button onClick={this.handleShowReport}>
+          {showReport ? "Hide Report" : "Show Report"}
         </button>
-      </div>
+        {showReport && (
+          <>
+            <h2>Repots</h2>
+            {this.state.cards.map((card) => (
+              <div>
+                Card number {card.title} unmounted {card.unmountCount} times
+              </div>
+            ))}
+          </>
+        )}
+      </>
     );
   }
 }
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,43 +1,18 @@
 import { Component } from "react";
-import ShowCard from "../showCard";
 
 export default class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = { isVisible: true, unmountCount: 0 };
   }
-  handleClick = () => {
-    this.setState({
-      isVisible: !this.state.isVisible,
-    });
-  };
-  handleUnmount = () => {
-    this.setState({ unmountCount: this.state.unmountCount + 1 });
-  };
+  componentWillUnmount() {
+    this.props.hanldeUnmountCount(this.props.title);
+  }
   render() {
-    const { title, showReport } = this.props;
-    const { isVisible, unmountCount } = this.state;
+    const { title, unmountCount } = this.props;
     return (
       <>
-        <div className="wrapper">
-          <button className="button" onClick={this.handleClick}>
-            {isVisible ? "Close" : "Show"}
-          </button>
-          {isVisible ? (
-            <ShowCard title={title} handleUnmount={this.handleUnmount} />
-          ) : (
-            ""
-          )}
-          {showReport ? (
-            <>
-              <div className="unmount">
-                Card number {title} unmounted {unmountCount} times
-              </div>
-            </>
-          ) : (
-            ""
-          )}
-        </div>
+        <div>{title}</div>
+        <div>{unmountCount}</div>
       </>
     );
   }
